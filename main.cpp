21c52ca4779ad0f8c4b8e15414b77fd602e7a5ef
Karim Karimov <karim2003@list.ru>
2020-09-14 09:52:38 +0000

    555

--- main.cpp
+++ main.cpp
@@ -4,11 +4,32 @@ int main() {
  int a;
  cout << "enter a ";
  cin >> a;
- if (a%2==0) {
-   cout << "chetnoe ";}
-   else
-   { cout << "nechetnoe ";
-   }
+ int b;
+ cout << "enter b ";
+ cin >> b;
+ int c;
+ cout << "enter c ";
+ cin >> c;
+ if ((a>=1) && (a<=3)) {
+   cout << "Число a лежит в интервале [1,3]" ;
+ }
+ else {
+   cout << "Число a не лежит в интервале [1,3]" ;
+ }
+ 
+ if ((b>=1) && (b<=3)) {
+   cout << "Число b лежит в интервале [1,3]" ;
+ }
+ else {
+   cout << "Число b не лежит в интервале [1,3]" ; 
+ }
+ 
+ if ((c>=1) && (c<=3)) {
+   cout << "Число c лежит в интервале [1,3]" ;
+ }
+ else {
+   cout << "Число c не лежит в интервале [1,3]" ;
+ }
  
 
 cout << " a = " << a;
